435b6e9829a8656c4c3017b61d0056c2c873e37d
edtan094 <etan094@gmail.com>
2022-04-21 14:23:46 -0700

    Created a react app and building some SASS files.  Put a navbar design on the page

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import Navbar from './Components/navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default App;
