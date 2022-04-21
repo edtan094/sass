d68b0e5dedb8ac4760d252dffe19b8ebbad68193
edtan094 <etan094@gmail.com>
2022-04-21 15:36:26 -0700

    Completed a body with the navbar using scss.

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import Navbar from './Components/navbar';
+import MainPage from './Components/mainpage'
 
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
+      <Navbar />
+      <MainPage />
     </div>
   );
 }
